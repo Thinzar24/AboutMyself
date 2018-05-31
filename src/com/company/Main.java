a64e1bed6e7bdc0d7a8062ca09b5352b8d89765b
Thinzar <tinzar.myathein@gmail.com>
2018-05-31 16:01:48 -0400

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("My Name is Thinzar");
+        System.out.println ("I am 35 ");
+	// write your code here
+    }
+}
